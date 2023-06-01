a654ae68c9b0f6ac33a358d7cf44027c03d2017d
tri-cong <imtricong@gmail.com>
2023-06-01 10:37:42 +0700

    cong update home page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,27 +1,106 @@
 import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
-import { Nav, NavItem, Navbar, NavbarBrand } from "reactstrap";
+import Logo from "../images/Logo.png";
+import styled from "styled-components";
+import {BsSearch} from "react-icons/bs";
 
 class Header extends Component {
     render() {
         return (
-            <>
-                <Navbar className="navbar-dark">
-                    <div className="container">
-                        <NavbarBrand>UME</NavbarBrand>
-                        <Nav navbar>
-                            <NavItem>
-                                <NavLink className="nav-link" to="/">Home</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to="/login">Login</NavLink>
-                            </NavItem>
-                        </Nav>
-                    </div>
-                </Navbar>
-            </>
+            <HeaderContainer>
+                <div className="HeadLogo">
+                    <img src={Logo} alt=""/>
+                </div>
+                <div className="HeadTxt">
+                    <div className="txtHome">Trang chủ</div>
+                    <div className="txtUs">Về chúng tôi</div>
+                </div>
+                <div className="HeadBtn">
+                    <button className="btn">Trở thành UNI</button>
+                    <BsSearch className="icon"/>
+                    <button className="btnLG">Đăng nhập</button>
+                    <button className="btnRS">Đăng ký</button>
+                </div>
+            </HeaderContainer>
         )
     }
 }
 
 export default Header;
+
+const HeaderContainer = styled.div`
+    background-color: #100C2E;
+    display: flex;
+
+    .HeadLogo {
+        padding-left: 30px;
+        img {
+            padding-top: 5px;
+            width: 45%;
+            border-radius: 27px;
+            cursor: pointer;
+        }
+    }
+
+    .HeadTxt {
+        padding-right: 37%;
+        padding-top: 13px;
+        display: flex;
+        .txtHome {
+            width: 100px;
+            color: white;
+            text-align: left;
+            cursor: pointer;
+        }
+    
+        .txtUs {
+            width: 150px;
+            color: white;
+            text-align: left;
+            cursor: pointer;
+        }
+    }
+
+    .HeadBtn {
+        display: flex;
+        padding-top: 7px;
+
+        .btn {
+            background-color: #2A235A;
+            border-color: #2A235A;
+            width: 170px;
+            height: 40px;
+            border-radius: 27px;
+            color: white;
+        }
+
+        .btnLG {
+            background-color: #2A235A;
+            border-color: #2A235A;
+            width: 130px;
+            height: 40px;
+            border-radius: 27px;
+            margin-right: 30px;
+            color: white;
+        }
+
+        .btnRS {
+            background-color: #5B8FB9;
+            border-color: #5B8FB9;
+            width: 130px;
+            height: 40px;
+            border-radius: 27px;
+            margin-right: 30px;
+            color: white;
+        }
+
+        .icon {
+            padding-top: 7px;
+            padding-left: 15px;
+            padding-right: 15px;
+            color: white;
+            width: 26px;
+            height: 26px;
+            cursor: pointer;
+        }
+    }
+`
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,14 +1,243 @@
 import React, { Component } from "react";
+import View from "../images/View.png";
+import Ava from "../images/Ava.png";
+import styled from "styled-components";
+import {AiFillStar} from "react-icons/ai";
+import {RiMoneyDollarCircleFill} from "react-icons/ri"
 
 class HomePage extends Component {
     render() {
         return(
-            <div>
-                <h1>Home</h1>
-            </div>
+            <HomeContainer>
+                <div className="ViewImg">
+                    <img src={View} alt=""/>
+                </div>
+                <div className="ViewCategory">
+                    <div className="Category">Danh mục</div>
+                    <div className="ViewBtn">
+                        <button className="btn">Ngôn ngữ Nhật</button>
+                        <button className="btn">Ngôn ngữ Anh</button>
+                        <button className="btn">Ngôn ngữ Trung</button>
+                        <button className="btn">Thiết kế đồ họa</button>
+                        <button className="btn">Kỹ thuật phần mềm</button>
+                        <button className="btn">Quản trị khách sạn</button>
+                        <button className="btn">Kinh doanh quốc tế</button>
+                    </div>
+                </div>
+                <div className="ViewTeacher">
+                    <div className="Teacher">Teacher</div>
+                    <div className="TeacherBtn">
+                        <button className="btn">
+                            <img src={Ava} alt=""/>
+                            <div className="txtTeacher">Thu Uyên</div>
+                            <div className="txtStar"><AiFillStar className="iconStar"/>5,0 (100)</div>
+                            <div className="txtMoney"><RiMoneyDollarCircleFill className="iconMoney"/>50.000/1h</div>
+                            <div className="txtClass">JPD201</div>
+                        </button>
+                        <button className="btn">
+                            <img src={Ava} alt=""/>
+                            <div className="txtTeacher">Thu Uyên</div>
+                            <div className="txtStar"><AiFillStar className="iconStar"/>5,0 (100)</div>
+                            <div className="txtMoney"><RiMoneyDollarCircleFill className="iconMoney"/>50.000/1h</div>
+                            <div className="txtClass">JPD201</div>
+                        </button>
+                        <button className="btn">
+                            <img src={Ava} alt=""/>
+                            <div className="txtTeacher">Thu Uyên</div>
+                            <div className="txtStar"><AiFillStar className="iconStar"/>5,0 (100)</div>
+                            <div className="txtMoney"><RiMoneyDollarCircleFill className="iconMoney"/>50.000/1h</div>
+                            <div className="txtClass">JPD201</div>
+                        </button>
+                        <button className="btn">
+                            <img src={Ava} alt=""/>
+                            <div className="txtTeacher">Thu Uyên</div>
+                            <div className="txtStar"><AiFillStar className="iconStar"/>5,0 (100)</div>
+                            <div className="txtMoney"><RiMoneyDollarCircleFill className="iconMoney"/>50.000/1h</div>
+                            <div className="txtClass">JPD201</div>
+                        </button>
+                    </div>
+                    <div className="TeacherBtn">
+                        <button className="btn">
+                            <img src={Ava} alt=""/>
+                            <div className="txtTeacher">Thu Uyên</div>
+                            <div className="txtStar"><AiFillStar className="iconStar"/>5,0 (100)</div>
+                            <div className="txtMoney"><RiMoneyDollarCircleFill className="iconMoney"/>50.000/1h</div>
+                            <div className="txtClass">JPD201</div>
+                        </button>
+                        <button className="btn">
+                            <img src={Ava} alt=""/>
+                            <div className="txtTeacher">Thu Uyên</div>
+                            <div className="txtStar"><AiFillStar className="iconStar"/>5,0 (100)</div>
+                            <div className="txtMoney"><RiMoneyDollarCircleFill className="iconMoney"/>50.000/1h</div>
+                            <div className="txtClass">JPD201</div>
+                        </button>
+                        <button className="btn">
+                            <img src={Ava} alt=""/>
+                            <div className="txtTeacher">Thu Uyên</div>
+                            <div className="txtStar"><AiFillStar className="iconStar"/>5,0 (100)</div>
+                            <div className="txtMoney"><RiMoneyDollarCircleFill className="iconMoney"/>50.000/1h</div>
+                            <div className="txtClass">JPD201</div>
+                        </button>
+                        <button className="btn">
+                            <img src={Ava} alt=""/>
+                            <div className="txtTeacher">Thu Uyên</div>
+                            <div className="txtStar"><AiFillStar className="iconStar"/>5,0 (100)</div>
+                            <div className="txtMoney"><RiMoneyDollarCircleFill className="iconMoney"/>50.000/1h</div>
+                            <div className="txtClass">JPD201</div>
+                        </button>
+                    </div>
+                    <div className="TeacherBtn">
+                        <button className="btn">
+                            <img src={Ava} alt=""/>
+                            <div className="txtTeacher">Thu Uyên</div>
+                            <div className="txtStar"><AiFillStar className="iconStar"/>5,0 (100)</div>
+                            <div className="txtMoney"><RiMoneyDollarCircleFill className="iconMoney"/>50.000/1h</div>
+                            <div className="txtClass">JPD201</div>
+                        </button>
+                        <button className="btn">
+                            <img src={Ava} alt=""/>
+                            <div className="txtTeacher">Thu Uyên</div>
+                            <div className="txtStar"><AiFillStar className="iconStar"/>5,0 (100)</div>
+                            <div className="txtMoney"><RiMoneyDollarCircleFill className="iconMoney"/>50.000/1h</div>
+                            <div className="txtClass">JPD201</div>
+                        </button>
+                        <button className="btn">
+                            <img src={Ava} alt=""/>
+                            <div className="txtTeacher">Thu Uyên</div>
+                            <div className="txtStar"><AiFillStar className="iconStar"/>5,0 (100)</div>
+                            <div className="txtMoney"><RiMoneyDollarCircleFill className="iconMoney"/>50.000/1h</div>
+                            <div className="txtClass">JPD201</div>
+                        </button>
+                        <button className="btn">
+                            <img src={Ava} alt=""/>
+                            <div className="txtTeacher">Thu Uyên</div>
+                            <div className="txtStar"><AiFillStar className="iconStar"/>5,0 (100)</div>
+                            <div className="txtMoney"><RiMoneyDollarCircleFill className="iconMoney"/>50.000/1h</div>
+                            <div className="txtClass">JPD201</div>
+                        </button>
+                    </div>
+                </div>
+            </HomeContainer>
         )
     }
 }
 
 export default HomePage;
 
+const HomeContainer = styled.div`
+    background-color: #03001C;
+    .ViewImg {
+        padding-top: 30px;
+        img {
+            width: 85%;
+            padding-left: 100px;
+            padding-right: 100px;
+            border-radius: 10%;
+        }
+    }
+
+    .ViewCategory {
+        .Category {
+            color: white;
+            font-weight: 600;
+            font-size: 30px;
+            padding-left: 100px;
+            padding-top: 50px;
+            padding-bottom: 50px;
+        }
+        .ViewBtn {
+            padding-left: 100px;
+            display: flex;
+            .btn {
+                background-color: #140F39;
+                color: white;
+                font-size: 40px;
+                font-weight: 600;
+                border-radius: 20px;
+                margin-right: 36px;
+                width: 150px;
+                height: 250px;
+                cursor: pointer;
+            }
+        }
+    }
+
+    .ViewTeacher {
+        .Teacher {
+            color: white;
+            font-weight: 600;
+            font-size: 30px;
+            padding-left: 100px;
+            padding-top: 50px;
+            padding-bottom: 50px;
+        }
+        .TeacherBtn {
+            padding-left: 100px;
+            padding-bottom: 100px;
+            display: flex;
+            .btn {
+                background-color: #140F39;
+                color: white;
+                font-size: 40px;
+                font-weight: 600;
+                border-radius: 10px;
+                margin-right: 36px;
+                margin-top: 50px;
+                width: 290px;
+                height: 290px;
+                cursor: pointer;
+                position: relative;
+
+                img {
+                    width: 180px;
+                    border-radius: 18px;
+                    position: absolute;
+                    top: -50px;
+                    left: 10%;
+                }
+
+                .txtTeacher {
+                    font-size: 25px;
+                    padding-top: 100px;
+                    padding-bottom: 5px;
+                    padding-left: 15px;
+                    text-align: left;
+                }
+                .txtStar {
+                    font-size: 20px;
+                    text-align: left;
+                    padding-bottom: 5px;
+                    padding-left: 15px;
+
+                    .iconStar {
+                        color: yellow;
+                    }
+                }
+                .txtMoney {
+                    font-size: 20px;
+                    text-align: left;
+                    padding-bottom: 5px;
+                    padding-left: 15px;
+
+                    .iconMoney {
+                        color: #5B8FB9;
+                    }
+                }
+
+                .txtClass {
+                    position: absolute;
+                    background-color: #5B8FB9;
+                    font-size: 20px;
+                    padding-top: 10px;
+                    padding-bottom: 10px;
+                    padding-left: 10px;
+                    padding-right: 10px;
+                    right: 0;
+                    bottom: 0;
+                    border-radius: 10px 0px 10px 0px;
+                }
+            }
+        }
+    }
+
+`
